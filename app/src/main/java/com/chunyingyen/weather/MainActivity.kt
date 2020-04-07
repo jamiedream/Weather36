22f5efe0cb72107e9e41043a762a11a720f98f9d
JamieY <jamie.yen@viwave.com>
2020-04-07 18:02:37 +0800

    init

---
+++ app/src/main/java/com/chunyingyen/weather/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.chunyingyen.weather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
