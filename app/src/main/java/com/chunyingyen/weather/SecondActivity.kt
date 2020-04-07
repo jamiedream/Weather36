b409bdd3d0e829310b273fbe8438d2da989698fe
JamieY <jamie.yen@viwave.com>
2020-04-07 18:03:44 +0800

    add second activity

---
+++ app/src/main/java/com/chunyingyen/weather/SecondActivity.kt
@@ -0,0 +1,15 @@
+package com.chunyingyen.weather
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondActivity: AppCompatActivity() {
+
+    private val TAG = "SecondActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+
+}
